6f4681df9ea60add82a5ea46273f0f212b0b1b70
Brittany M. Morris <brittmmendez@gmail.com>
2018-05-22 21:48:39 -0400

    added CRUD nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+      resources :trails do
+        resources :comments
+      end
+    end
+
+    post '/login', to: "sessions#create"
 end
